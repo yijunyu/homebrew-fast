c8cd0f085dbfc325d906230922f47e6e607f9074
Yijun Yu <yijun.yu@gmail.com>
2017-04-04 17:05:52 +0100

    added the formula for MRMC

---
+++ Formula/mrmc.rb
@@ -0,0 +1,35 @@
+class Gsl < Formula
+  desc "Markov Reward Model Checker"
+  homepage "http://www.mrmc-tool.org/"
+  url "https://github.com/yijunyu/homebrew-fast/raw/master/src/mrmc-1.5.tar.gz"
+  sha256 ""
+
+  depends_on "gsl"
+
+  def install
+    system "make", "all"
+  end
+
+  test do
+    system bin/"mrmc", "-h"
+  end
+end
+_END
+diff -r -u a/mrmc_src_v1.5/makefile b/mrmc_src_v1.5/makefile
+--- a/mrmc_src_v1.5/makefile	2011-01-12 11:59:28.000000000 +0000
++++ b/mrmc_src_v1.5/makefile	2017-04-04 17:01:48.000000000 +0100
+@@ -73,6 +73,14 @@
+ all: bin lib
+ 	$(MAKE) -C obj $@
+ 
++DESTDIR=
++prefix=/usr/local
++
++install: 
++	mkdir -p $(DESTDIR)$(prefix)/bin
++	install -m 0755 bin/mrmc $(DESTDIR)$(prefix)/bin/mrmc
++	install -m 0755 lib/mrmc.a $(DESTDIR)$(prefix)/lib/mrmc.a
++
+ lint:
+ 	$(MAKE) -C obj $@
+ 
