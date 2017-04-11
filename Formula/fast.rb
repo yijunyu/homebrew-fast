35464c282bd6f0093188b50246a6044a8fb3d12c
Yijun Yu <yijun.yu@gmail.com>
2017-04-11 08:23:55 +0100

    update fast bottle

--- Formula/fast.rb
+++ Formula/fast.rb
@@ -2,7 +2,13 @@ class Fast < Formula
   desc "Flattening Abstract Syntax Trees"
   homepage "https://github.com/yijunyu/fast"
   url "https://github.com/yijunyu/fast/archive/v0.0.1.tar.gz"
-  sha256 "5cb48dd8917124e50e9757abcf1e717775ebcdb008cc4ddeae8202cf073926d4"
+  sha256 "0bb6ddc236272467a32958e7c46384b4b6d9ef18c5bf612d1d30661caad03eda"
+
+  bottle do
+    root_url "https://github.com/yijunyu/fast/releases/download/v0.0.1"
+    cellar :any
+    sha256 "398c812895180e16844cf9e4f8bf8fe478820a93655342320a29d1f2a48fcc09" => :sierra
+  end
 
   depends_on "cmake" => :build
   depends_on "flatbuffers" => :build
@@ -25,44 +31,6 @@ public class Hello {
 }
     EOS
 
-    (testpath/"Hello-result.xml").write <<-EOS
-<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
-<unit xmlns="http://www.srcML.org/srcML/src" revision="0.9.5" language="Java" filename="Hello.java">
-<class><specifier>public</specifier> class <name>Hello</name> <block>{
-    <function><specifier>public</specifier> <specifier>static</specifier> <type><name>void</name></type> <name>main</name><parameter_list>(<parameter><decl><type><name>String</name></type> <name><name>args</name><index>[]</index></name></decl></parameter>)</parameter_list> <block>{
-        <expr_stmt><expr><call><name><name>System</name><operator>.</operator><name>out</name><operator>.</operator><name>println</name></name><argument_list>(<argument><expr><literal type="string">"Hello, world!"</literal></expr></argument>)</argument_list></call></expr>;</expr_stmt>
-    }</block></function>
-}</block></class>
-</unit>
-    EOS
-
-    (testpath/"Hello-fbs-result.xml").write <<-EOS
-<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
-<unit xmlns="http://www.srcML.org/srcML/src" xmlns:java="http://www.srcML.org/srcML/java" revision="0.9.5" language="JAVA" filename="Hello.java"><class><specifier>public</specifier> class <name>Hello</name><block>{
-    <function><specifier>public</specifier><specifier>static</specifier><type><name>void</name></type><name>main</name><parameter_list>(<parameter><decl><type><name>String</name></type><name><name>args</name><index>[]</index></name></decl></parameter>)</parameter_list><block>{
-        <expr_stmt><expr><call><name><name>System</name><operator>.</operator><name>out</name><operator>.</operator><name>println</name></name><argument_list>(<argument><expr><literal>"Hello, world!"</literal></expr></argument>)</argument_list></call></expr>;</expr_stmt>
-    }</block></function>
-}</block></class></unit>
-    EOS
-
-    (testpath/"Hello-pb-result.xml").write <<-EOS
-<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
-<unit xmlns="http://www.srcML.org/srcML/src" xmlns:="http://www.srcML.org/srcML/" revision="0.9.5" language="" filename="Hello.java"><class><specifier>public</specifier> class <name>Hello</name><block>{
-    <function><specifier>public</specifier><specifier>static</specifier><type><name>void</name></type><name>main</name><parameter_list>(<parameter><decl><type><name>String</name></type><name><name>args</name><index>[]</index></name></decl></parameter>)</parameter_list><block>{
-        <expr_stmt><expr><call><name><name>System</name><operator>.</operator><name>out</name><operator>.</operator><name>println</name></name><argument_list>(<argument><expr><literal type="">"Hello, world!"</literal></expr></argument>)</argument_list></call></expr>;</expr_stmt>
-    }</block></function>
-}</block></class></unit>
-    EOS
-
-    fork do
-      exec "#{bin}/srcml", "Hello.java", "-o", "Hello.xml"
-      exec "#{bin}/fast", "Hello.xml", "Hello.fbs"
-      exec "#{bin}/fast", "Hello.fbs", "Hello-fbs.xml"
-      exec "#{bin}/fast", "Hello.xml", "Hello.pb"
-      exec "#{bin}/fast", "Hello.pb", "Hello-pb.xml"
-      exec "diff", "Hello.xml", "Hello-result.xml"
-      exec "diff", "Hello-fbs.xml", "Hello-fbs-result.xml"
-      exec "diff", "Hello-pb.xml", "Hello-pb-result.xml"
-    end
+    exec "#{bin}/fast", "Hello.java", "Hello.pb"
   end
 end
