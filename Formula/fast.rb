67d63368e6faa8a0e7c037a5001e8434b93c8961
Yijun Yu <yijun.yu@gmail.com>
2017-04-11 08:51:30 +0100

    fast:update the bottle hash

--- Formula/fast.rb
+++ Formula/fast.rb
@@ -2,12 +2,12 @@ class Fast < Formula
   desc "Flattening Abstract Syntax Trees"
   homepage "https://github.com/yijunyu/fast"
   url "https://github.com/yijunyu/fast/archive/v0.0.1.tar.gz"
-  sha256 "51c68e99a9d78b889a2d9ac6dab11635a185d63b325840ce394419732affc30d"
+  sha256 "f9427929de1c1249770f0354a458c991d3c24719264bc1eb1befd0eae36fc448"
 
   bottle do
     root_url "https://github.com/yijunyu/fast/releases/download/v0.0.1"
     cellar :any
-    sha256 "b5770c71438d7a9e6ef8fd07c460281ffefbd21e70250caa44e06da5fd6b3cd8" => :sierra
+    sha256 "30d425fc9e712821dcd49e8accae0dbdb36ab411d1dc9f2322d51d2babd2b721" => :sierra
   end
 
   depends_on "cmake" => :build
