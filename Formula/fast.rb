e6815788d597fdd46ed86260a1b12ea5b3abde05
Yijun Yu <yijun.yu@gmail.com>
2017-06-09 14:56:05 +0100

    update fast's homebrew package

--- Formula/fast.rb
+++ Formula/fast.rb
@@ -8,7 +8,7 @@ class Fast < Formula
     root_url "https://github.com/yijunyu/fast/releases/download/v0.0.1"
     cellar :any
     sha256 "097a0d9bb1ab41b80e18591e861abeb6f397d3b93eae261dfa73bafd77eab789" => :sierra
-    sha256 "3dbc1b3e42ca6db9c8711b205770c9224aebc30df50a222a2432385e962e2a96" => :el_capitan
+    sha256 "a93036dcfad5956d076eb5ea03ad91d74fd7a85e706de153daac1bf31b32fc08" => :el_capitan
   end
 
   depends_on "flatbuffers" => :build
