2e320da7e92ca2fb89aa87e6f8a65bff007b0502
Yijun Yu <yijun.yu@gmail.com>
2017-06-18 15:01:13 +0100

    update fast's homebrew package

--- Formula/fast.rb
+++ Formula/fast.rb
@@ -2,7 +2,7 @@ class Fast < Formula
   desc "Flattening Abstract Syntax Trees"
   homepage "https://github.com/yijunyu/fast"
   url "https://github.com/yijunyu/fast/archive/v0.0.1.tar.gz"
-  sha256 "66597c7bb123d86db88ab72461f3206d68f428ff77db2269548de411e3c59e18"
+  sha256 "24d619a6859605f01a71456e40227edd00be4fbf50ca31462dfb90d3c6651586"
 
   bottle do
     root_url "https://github.com/yijunyu/fast/releases/download/v0.0.1"
