e0f3c8930b387680ab164da4b172e0608e0ad5a9
Yijun Yu <yijun.yu@gmail.com>
2017-07-06 10:53:23 +0100

    update fast's homebrew package

--- Formula/fast.rb
+++ Formula/fast.rb
@@ -7,7 +7,7 @@ class Fast < Formula
   bottle do
     root_url "https://github.com/yijunyu/fast/releases/download/v0.0.3"
     cellar :any
-    sha256 "" => :sierra
+    sha256 "547fe057f768909efff4a5d9eb9204e8de11e67c797d3acfd0ce96b1b732b9a7" => :sierra
     sha256 "1e77ab41415d37827738aac32ab28ee756a78befbb9576f0f4b3ef8ca6cac815" => :el_capitan
   end
 
