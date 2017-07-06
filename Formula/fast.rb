6149ae88e14a3957b16f51db98fcc61ee2c55001
Yijun Yu <yijun.yu@gmail.com>
2017-07-06 09:30:31 +0100

    update fast's homebrew package

--- Formula/fast.rb
+++ Formula/fast.rb
@@ -16,8 +16,8 @@ class Fast < Formula
   depends_on "protobuf" => :build
   depends_on "srcml" => :run
   depends_on "libxml2"
-  depends_on "antlr"
-  depends_on "antlr4-cpp-runtime"
+  depends_on "antlr" => :build
+  depends_on "antlr4-cpp-runtime" => :build
   depends_on "lcov"
 
   def install
